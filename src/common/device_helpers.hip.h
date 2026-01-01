28066ddfab32bdf155d91a60eeff71571e3312f9
root <root@ctr-cx66-mi300x-02.amd.com>
2026-01-01 20:24:52 +0000

    Shared mem not getting allocated fix, fixes the test Histogram.GPUHistCategorical

--- src/common/device_helpers.hip.h
+++ src/common/device_helpers.hip.h
@@ -171,11 +171,9 @@ inline size_t MaxSharedMemory(int device_idx) {
 
 inline size_t MaxSharedMemoryOptin(int device_idx) {
   int max_shared_memory = 0;
-#if 0 /* CUDA Only */
   dh::safe_cuda(hipDeviceGetAttribute
-                (&max_shared_memory, hipDeviceAttributeSharedMemPerBlockOptin,
+                (&max_shared_memory, hipDeviceAttributeMaxSharedMemoryPerBlock,
                  device_idx));
-#endif
   return static_cast<std::size_t>(max_shared_memory);
 }
 
@@ -1198,3 +1196,4 @@ class LDGIterator {
   }
 };
 }  // namespace dh
+
