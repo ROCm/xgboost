3215525a1b362262ea5b07281a75b12973bcee32
Karthik Kashyap Thatipamula <kthatipa@amd.com>
2026-01-14 04:17:03 -0500

    Skip test failures with cudf incompatability on ROCm 6.4

--- tests/python-gpu/test_gpu_ordinal.py
+++ tests/python-gpu/test_gpu_ordinal.py
@@ -26,8 +26,10 @@ from xgboost.testing.ordinal import (
     run_validation,
 )
 
-pytestmark = pytest.mark.skipif(**tm.no_multiple(tm.no_arrow(), tm.no_cudf()))
-
+pytestmark = [
+    pytest.mark.skipif(**tm.no_multiple(tm.no_arrow(), tm.no_cudf())),
+    pytest.mark.skipif(**tm.is_rocm_64()),
+]
 
 def test_cat_container() -> None:
     run_cat_container("cuda")
